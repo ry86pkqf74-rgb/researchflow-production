9b95f627009be799803c28a0a43d4d64d0b70695
ry86pkqf74-rgb <logan.glosser@gmail.com>
2026-01-18 07:44:38 -0500

    feat(manuscript-engine): add services index and test config

---
+++ packages/manuscript-engine/src/services/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Manuscript Engine Services
+ * Barrel export for all services
+ */
+
+export * from './phi-guard.service';
+export * from './data-mapper.service';
+export * from './data-tagger.service';
+export * from './version-control.service';
