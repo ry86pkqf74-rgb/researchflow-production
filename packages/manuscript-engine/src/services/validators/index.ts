251aea364ce4d0763ef8bcda493329562f76ac55
ry86pkqf74-rgb <Logan.glosser@gmail.com>
2026-01-23 14:45:53 -0500

    feat(phase-3): Add validators module exports

---
+++ packages/manuscript-engine/src/services/validators/index.ts
@@ -0,0 +1,19 @@
+/**
+ * Validators Module
+ * Phase 3.2: Manuscript section validators
+ */
+
+export {
+  validateWordBudget,
+  validateManuscript,
+  countWords,
+  countCharacters,
+  countParagraphs,
+  countSentences,
+  getJournalBudgets,
+  mergeBudgets,
+  DEFAULT_BUDGETS,
+  JOURNAL_PRESETS,
+  type WordBudget,
+  type WordBudgetValidation
+} from './word-budget';
